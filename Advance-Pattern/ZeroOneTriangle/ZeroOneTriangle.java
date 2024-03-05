db401f440005a466b47a5aea7e325ba3e1d6fe70
Azim Afnaan <azim.afnaan@hotmail.com>
2024-03-05 17:45:55 +0600

    zero one triangle

---
+++ Advance-Pattern/ZeroOneTriangle/ZeroOneTriangle.java
@@ -0,0 +1,29 @@
+/*
+Zero One Triangle
+
+1
+01
+101
+0101
+10101
+
+*/
+
+public class ZeroOneTriangle {
+    public static void triangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print(1);
+                } else {
+                    System.out.print(0);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        triangle(5);
+    }
+}
